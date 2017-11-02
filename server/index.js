e2ad4c1bd7b623b5716d2d96b9875ebd795c3018
Baeron <baranoffei@gmail.com>
2017-11-02 18:46:43 +0200

    Render main react component

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 var app = express();
 
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
 app.get('/*', (req, res) => {
-    res.sendfile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(3000, () => console.log('Server start on port 3000'));
