bab0ffe94b0a9e4743cfe23a369d147e942d8ebf
Baeron <baranoffei@gmail.com>
2017-11-03 15:16:28 +0200

    Adds server side validation

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
 
 import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpackConfig from '../webpack.config.dev';
 
-var app = express();
+import users from './routes/users';
 
-var compiler = webpack(webpackConfig);
+let app = express();
+
+app.use(bodyParser.json());
+
+app.use('/api/users', users)
+
+const compiler = webpack(webpackConfig);
 
 app.use(webpackMiddleware(compiler, {
     hot: true,
